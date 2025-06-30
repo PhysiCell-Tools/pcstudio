dbd9bf2c16634251cd86be31bb219d438e829274
Randy Heiland <randy.heiland@gmail.com>
2025-06-30 14:44:45 -0400

    hack for np.min initial

--- bin/ics_tab.py
+++ bin/ics_tab.py
@@ -2614,10 +2614,14 @@ class ICs(QWidget):
             max_val = float(self.substrate_color_pars[self.substrate_combobox.currentText()]["cmax"])
         else:
             min_val = min(float(self.substrate_set_value.text()),np.min(self.current_substrate_values))
+            # min_pos_val = np.min(self.current_substrate_values[self.current_substrate_values>0],initial=float(self.substrate_set_value.text()))
             try:
                 min_pos_val = np.min(self.current_substrate_values[self.current_substrate_values>0],initial=float(self.substrate_set_value.text()))
             except:
                 min_pos_val = np.min(self.current_substrate_values[self.current_substrate_values>0])
+                if float(self.substrate_set_value.text()) < min_pos_val:
+                    min_pos_val = float(self.substrate_set_value.text())
+                pass
             max_val = max(float(self.substrate_set_value.text()),np.max(self.current_substrate_values))
         if (min_pos_val>0) and ((self.substrate_color_pars[self.substrate_combobox.currentText()]["scale"]=="log") or (self.substrate_color_pars[self.substrate_combobox.currentText()]["scale"]=="auto" and max_val > 100*min_pos_val)):
             self.substrate_plot.set_norm(matplotlib.colors.LogNorm(vmin=min_pos_val, vmax=max_val))
